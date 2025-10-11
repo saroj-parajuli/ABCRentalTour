54ca672a78f1a8f3a426188f98e74ad4acd5087d
saroj-parajuli <saroj.parajuli91@gmail.com>
2025-10-11 17:47:29 -0500

    footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header-component/header-component';
+import { FooterComponent } from './footer-component/footer-component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.html',
   styleUrls: ['./app.scss']
 })
