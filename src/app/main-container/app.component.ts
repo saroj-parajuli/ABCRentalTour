54aec20add7822fb8b4e4a4fcbdcc4c799e41f5c
saroj-parajuli <saroj.parajuli91@gmail.com>
2025-10-13 11:55:15 -0500

    updated

--- src/app/main-container/app.component.ts
+++ src/app/main-container/app.component.ts
@@ -2,11 +2,18 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from '../components/footer-component/footer-component';
 import { HeaderComponent } from '../components/header-component/header-component';
-
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 @Component({
   selector: 'app-container',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
